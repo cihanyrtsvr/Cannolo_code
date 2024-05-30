0325715387a40974996134e901dce7f4e14cd49e
Cihan Yurtsever <67927062+cihanyrtsvr@users.noreply.github.com>
2024-05-30 22:40:50 +0200

    first problem solved

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main(){
+    string str = "pqr3stu8vwx";
+    int first = -1, second =-1, total;
+    vector<int> result;
+
+    ifstream file("input.txt");
+
+    string line;
+    while(getline(file, line)){
+
+    for ( char c : line){
+        if(c>=48 && c <= 57){
+            first<0 ? first = c-48 : second = c-48 ;
+            
+        }
+    }
+    second < 0 ? second = first : second=second ;
+          
+    
+
+    total += (first * 10) + second ;
+
+    first = -1;
+    second = -1 ;
+    
+    }
+
+    cout<< total<<endl;
+    return 0;
+}
+
